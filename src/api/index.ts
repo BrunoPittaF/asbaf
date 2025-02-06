0ccd891bf71e2d73d7bd194ab2e60626b82df8f8
brunopittaf <caiobrunopittaf@gmail.com>
2025-02-06 19:52:15 -0300

    feat:  url deploy backend

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-export const baseURL = import.meta.env.REACT_APP_BASE_URL || 'http://localhost:8080'
+//local
+// export const baseURL = import.meta.env.REACT_APP_BASE_URL || 'http://localhost:8080'
+//prod
+export const baseURL = 'https://backend-asbaf.vercel.app/'
+
 
 const api = axios.create({
   baseURL: baseURL,
