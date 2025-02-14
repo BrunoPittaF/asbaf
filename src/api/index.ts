2154ab25fad4305a84e11b720c519984d1f37640
brunopittaf <caiobrunopittaf@gmail.com>
2025-02-14 11:30:18 -0300

    feat: corrigindo url de cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 //local
 // export const baseURL = import.meta.env.REACT_APP_BASE_URL || 'http://localhost:8080'
 //prod
-export const baseURL = 'https://backend-asbaf.vercel.app/'
+export const baseURL = 'https://backend-asbaf.vercel.app'
 
 
 const api = axios.create({
